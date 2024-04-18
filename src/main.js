186a75c29b4aba8588ea7a5b2489b198053cd33e
franzley <franzleybonhomme003@gmail.com>
2024-04-18 12:11:10 -0400

    removed unecessary css and html | changed images

--- src/main.js
+++ src/main.js
@@ -12,8 +12,6 @@ import '../styles/utils.css'
 
 import mobileNav from './utils/mobile-nav'
 import themeToggle from './utils/dark-mode'
-import lazyLoading from './utils/lazy-loading'
 
 mobileNav();
 themeToggle();
-// lazyLoading();
