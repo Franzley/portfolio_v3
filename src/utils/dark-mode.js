79945bdc6bf4537a7e4b049fa62b6ed670872f83
franzley <franzleybonhomme003@gmail.com>
2024-04-22 16:19:32 -0400

    Added link to imports due to css not loading on page reload and added more comments for code clarity

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,4 +1,5 @@
 const themeToggle = ()=>{
+    // Select all elements that toggles the theme
     const themeToggleBtns = document.querySelectorAll('.theme-toggle');
 
     // State
@@ -7,18 +8,19 @@ const themeToggle = ()=>{
     // on mount
     theme && document.body.classList.add(theme)
     
-    // handlers
     const handleThemeToggle = ()=>{
+        // Assign to the body the light mode
         document.body.classList.toggle('light-mode');
             if(document.body.classList.contains('light-mode')){
                 localStorage.setItem('theme', 'light-mode');
             }else{
+                // if light mode is currently on, remove light mode
                 localStorage.removeItem('theme');
                 document.body.removeAttribute('class')
             }
     }
 
-    // Events
+    // Check if theme btn has been clicked and call method
     themeToggleBtns.forEach((toggle)=>{
         toggle.addEventListener('click',handleThemeToggle)
     })
--- src/utils/mobile-nav.js
+++ src/utils/mobile-nav.js
@@ -1,20 +1,29 @@
 const mobileNav = () => {
+    // select element through its class name
     const headerBars = document.querySelector('.header-bars');
     const mobileNav = document.querySelector('.mobile-nav');
+    // select all elements under class name
     const mobileLink = document.querySelectorAll('.mobile-nav-link');
 
+    // Initialize mobile navigation menu as not active
     let isMobileNavOpen = false;
+
     headerBars.addEventListener('click', () => {
+        // Set the mobile navigation menu status to the opposite of its current state
         isMobileNavOpen = !isMobileNavOpen;
+
+        // if state is active
         if(isMobileNavOpen){
             mobileNav.style.display = 'flex';
             document.body.style.overflowY = 'hidden';
         }else{
+            // if state is not active
             mobileNav.style.display = 'none';
             document.body.style.overflowY = 'auto';
         }
     });
 
+    // Close mobile navigation menu once menu item is clicked
     mobileLink.forEach((link)=>{
         link.addEventListener('click', ()=>{
             isMobileNavOpen = false
